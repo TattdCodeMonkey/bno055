538e56890c22d5a35c8c1ab7877b8bb1e57b25ff
Rodney Norris <Rodney.Norris@gmail.com>
2016-03-05 21:36:45 -0600

    updated version post-release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bno055.Mixfile do
 
   def project do
     [app: :bno055,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: ">= 1.0.0 and < 2.0.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
