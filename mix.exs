7f389ad6787812d3c51bf17686a120c96aee6977
Rodney Norris <rodney@tattdcodemonkey.com>
2020-09-01 12:00:43 -0500

    fix mix.exs

--- mix.exs
+++ mix.exs
@@ -7,9 +7,9 @@ defmodule Bno055.Mixfile do
      elixir: ">= 1.0.0 and < 2.0.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
-     description: description,
-     package: package,
+     deps: deps(),
+     description: description(),
+     package: package(),
    ]
   end
 
